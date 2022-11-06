bb263de03560399b9d57ba6842ea3ca795f5f310
rattsl <r4ttsl@gmail.com>
2022-11-06 17:34:54 +0900

    add socketmode

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import { App } from "@slack/bolt";
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN,
 });
 
 app.use(async ({ next }) => {
