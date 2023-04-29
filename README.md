220692d0a485a3735f1f672b1cf19621bcc95f70
h.kakiuchi <h.kakiuchi@no4.co.jp>
2023-04-30 03:18:36 +0900

    fix

---
+++ README.md
@@ -0,0 +1,17 @@
+## 参考
+https://slack.dev/bolt-js/ja-jp/tutorial/getting-started
+
+## 開発環境構築
+
+1. 以下のコマンドを実行して.envファイル複製
+`cp .env.development .env`
+
+2. .envに環境変数を設定する
+各環境変数にクレデンシャルな情報を追加
+
+```
+SLACK_BOT_TOKEN={トークン}
+SLACK_SIGNING_SECRET={シークレットトークン}
+```
+
+
--- src/app.ts
+++ src/app.ts
@@ -11,41 +11,31 @@ app.use(async ({ next }) => {
   await next();
 });
 
-// Listens to incoming messages that contain "hello"
-// app.message(
-//   "hello",
-//   ({ message, say }: MessageEventMiddlewar) => {
-//     // say() sends a message to the channel where the event was triggered
-//     say(`Hey there <@${message.user}>!`);
-//   }
-// );
-
-app.message("hello", async ({ message, say }) => {
-  // Filter out message events with subtypes (see https://api.slack.com/events/message)
-  if (message.subtype === undefined || message.subtype === "bot_message") {
-    // say() sends a message to the channel where the event was triggered
-    await say({
-      blocks: [
-        {
-          type: "section",
-          text: {
-            type: "mrkdwn",
-            text: `Hey there <@${message.channel}>!`,
-          },
-          accessory: {
-            type: "button",
-            text: {
-              type: "plain_text",
-              text: "Click Me",
-            },
-            action_id: "button_click",
-          },
-        },
-      ],
-      text: `Hey there <@${message.channel}>!`,
-    });
+app.message(
+  "health check",
+  async ({ say }) => {
+    await say(':wave:');
   }
-});
+);
+
+// app.message("hello", async ({ message, say }) => {
+//   // Filter out message events with subtypes (see https://api.slack.com/events/message)
+//   if (message.subtype === undefined || message.subtype === "bot_message") {
+//     // say() sends a message to the channel where the event was triggered
+//     await say({
+//       blocks: [
+//         {
+//           type: "section",
+//           text: {
+//             type: "mrkdwn",
+//             text: `Hey there <@${message.channel}>!`,
+//           },
+//         },
+//       ],
+//       text: `Hey there <@${message.channel}>!`,
+//     });
+//   }
+// });
 
 (async () => {
   // Start your app
